3cf2beb1dcb5b1015c00306b5357ed4c7b6a76ea
LimaZurmati <urzalalima@gmail.com>
2022-08-26 21:13:53 +0000

    Add cases for switch

--- assets/js/js/script.js
+++ assets/js/js/script.js
@@ -1,2 +1,27 @@
 let calculator = document.getElementById('calculator');
+
 let operators = Array.from(document.getElementsByClassName('operator'));
+
+operators.map( operator => {
+    operator.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                calculator.innerText = '';
+                break;
+            case '=':
+                try{
+                    calculator.innerText = eval(calculator.innerText);
+                } catch {
+                    calculator.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (calculator.innerText){
+                    calculator.innerText = calculator.innerText.slice(0, -1);
+                }
+                break;
+            default:
+                calculator.innerText += e.target.innerText;
+        }
+    });
+});
